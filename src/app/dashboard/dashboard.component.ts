7604d477a603bef93946b8e233efdc05a1f01399
neoThobs <neo.thobz@gmail.com>
2021-06-17 11:17:18 +0200

    added business rules and updated UI

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,11 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { Account } from '../account';
 import { AccountsService } from '../service/accounts.service';
 
+enum ACCOUNT_TYPE {
+  CHEQUE = 'cheque',
+  SAVINGS = 'savings'
+}
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
 export class DashboardComponent implements OnInit {
 
   accounts: Account[] = [];
@@ -19,14 +24,36 @@ export class DashboardComponent implements OnInit {
   }
 
   getAccounts(): void {
-    this.accountService.getAccounts().subscribe(accounts => {
+    this.accountService.getAccounts().subscribe((accounts) => {
       this.accounts = accounts
 
       for (let account of accounts) {
         this.accountBalance += +account.balance;
       }
-    }
+    },
+      (error) => {
+
+      }
     );
+  }
+
+  getAccountStatus(account: Account): string {
+
+    if ((account.account_type === ACCOUNT_TYPE.SAVINGS) && +account.balance > 0) {
+      return 'waves-effect waves-light btn';
+    }
+    else if ((account.account_type === ACCOUNT_TYPE.SAVINGS) && +account.balance <= 0) {
+      return 'btn disabled';
+    }
+    else if ((account.account_type === ACCOUNT_TYPE.CHEQUE) && +account.balance > -500) {
+      return 'waves-effect waves-light btn';
+    }
+    else if ((account.account_type === ACCOUNT_TYPE.CHEQUE) && +account.balance < -500) {
+      return 'btn disabled';
+    }
+    else {
+      return 'waves-effect waves-light btn';
+    }
   }
 
   withdraw(): void {
